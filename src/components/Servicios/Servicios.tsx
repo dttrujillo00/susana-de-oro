3aaedd1126be2571333fbf98b178c2d5387c87a2
Daniel Tamayo <dttrujillo00@gmail.com>
2024-03-21 09:20:20 -0400

    iconos de los servicios agregados

--- src/components/Servicios/Servicios.tsx
+++ src/components/Servicios/Servicios.tsx
@@ -7,6 +7,7 @@ export const Servicios = () => {
 
         <article className='servicio-card'>
 
+          <i className="fa-solid fa-scale-balanced fa-2xl"></i>
           <h3>Asesoramiento Legal</h3>
           <p>Estoy disponible para responder a tus consultas legales, revisar contratos y brindarte orientación en cualquier asunto legal que puedas enfrentar.</p>
 
@@ -14,6 +15,7 @@ export const Servicios = () => {
 
         <article className='servicio-card'>
           
+          <i className="fa-solid fa-file-signature fa-2xl"></i>
           <h3>Elaboración y Revisión de Documentos</h3>
           <p>Puedo redactar y revisar contratos, acuerdos comerciales, políticas internas y otros documentos legales para garantizar su validez y cumplimiento normativo.</p>
 
@@ -21,6 +23,7 @@ export const Servicios = () => {
 
         <article className='servicio-card'>
           
+          <i className='fa-solid fa-gavel fa-2xl'></i>
           <h3>Cumplimiento Normativo</h3>
           <p>Te ayudaré a mantener el cumplimiento de las leyes y regulaciones aplicables a tu industria, evitando posibles sanciones y riesgos legales.</p>
 
