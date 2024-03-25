b84600027eeb63eeaee32d1328bc9f7813efcc2e
Daniel Tamayo <dttrujillo00@gmail.com>
2024-03-25 10:01:32 -0400

    correcion

--- src/services/getNews.ts
+++ src/services/getNews.ts
@@ -1,7 +1,7 @@
 import { Article } from "../interfaces/article";
 
 
-export const getNews = async () => {
+export const getNews = async (): Promise<Article[]> => {
     
     const response = await fetch('https://server-susana-de-oro.onrender.com/news')
     const { body } = await response.json()
