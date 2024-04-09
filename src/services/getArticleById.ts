0ce0bec94111f722fc68d0d6388342761cc59e7d
Daniel Tamayo <dttrujillo00@gmail.com>
2024-04-09 13:43:23 -0400

    modificada ruta de obtener solo un articulo

--- src/services/getArticleById.ts
+++ src/services/getArticleById.ts
@@ -2,7 +2,7 @@ import { Article } from "../interfaces";
 
 export const getArticleById = async (id: string): Promise<Article> => {
     
-    const response = await fetch(`https://server-susana-de-oro.onrender.com/news/${id}`)
+    const response = await fetch(`https://server-susana-de-oro.onrender.com/public-news/${id}`)
     const { body } = await response.json()
     return body.response[0];
 
